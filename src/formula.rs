905c67b0471615c0be809b8048ff9396b3c5fdf3
Abdul Zreika <azre6702@uni.sydney.edu.au>
2019-08-10 16:48:22 -0500

    Added variants for formula enum.

--- src/formula.rs
+++ src/formula.rs
@@ -1,2 +1,6 @@
-pub enum Formula {
+pub enum Formula<'a> {
+    And(Box<Formula<'a>>, Box<Formula<'a>>),
+    Or(Box<Formula<'a>>, Box<Formula<'a>>),
+    Not(Box<Formula<'a>>),
+    Var(&'a str),
 }
--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,17 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::formula::Formula::*;
+
+    #[test]
+    fn simple_construct() {
+        let p = Box::new(Var("p"));
+        let q = Box::new(Var("q"));
+        let r = Box::new(Var("r"));
+
+        let conj = Box::new(And(p, q));
+        let disj = Box::new(Or(conj, r));
+
+        let not = Box::new(Not(disj));
+    }
 }
