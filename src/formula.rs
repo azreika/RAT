f0d5816c1d91e0f456cfc4b0ce463d453518f411
Abdul Zreika <azre6702@uni.sydney.edu.au>
2019-08-14 21:51:09 -0500

    Implemented reduction method for formulas.
    Reduces a formula into a vector of labelled simple subformulas.

--- src/formula.rs
+++ src/formula.rs
@@ -38,9 +38,65 @@ impl fmt::Debug for Formula {
     }
 }
 
+fn gen_var(idx: usize) -> String {
+    format!("@var{}", idx)
+}
+
 impl Formula {
     pub fn get_cnf(&self) -> Formula {
-        Formula::Var(format!("x:{}", self.count_nontrivial_subformulas()))
+        Formula::Var(gen_var(0))
+        // Formula::Var(format!("x:{}", self.count_nontrivial_subformulas()))
+    }
+
+    pub fn reduce(&self, idx: usize) -> Vec<(String, Formula)> {
+        let mut assignments: Vec<(String, Formula)> = Vec::new();
+        match self {
+            Formula::Not(ref sub) => {
+                let mut reduced = sub.reduce(idx+1);
+                let subvariable = Formula::Var(reduced[0].0.clone());
+                assignments.push((
+                        gen_var(idx),
+                        Formula::Not(Box::new(subvariable))));
+                assignments.append(&mut reduced);
+            },
+            Formula::Var(ref name) => {
+                assignments.push((
+                        name.clone(),
+                        Formula::Var(name.clone())));
+            },
+            Formula::Or(ref left, ref right) => {
+                let mut left_reduced = left.reduce(idx+1);
+                let left_var = Formula::Var(left_reduced[0].0.clone());
+
+                let mut right_reduced = right.reduce(idx+left_reduced.len()+1);
+                let right_var = Formula::Var(right_reduced[0].0.clone());
+
+                assignments.push((
+                        gen_var(idx),
+                        Formula::Or(Box::new(left_var), Box::new(right_var))));
+                assignments.append(&mut left_reduced);
+                assignments.append(&mut right_reduced);
+            },
+            Formula::And(ref left, ref right) => {
+                let mut left_reduced = left.reduce(idx+1);
+                let left_var = Formula::Var(left_reduced[0].0.clone());
+
+                let mut right_reduced = right.reduce(idx+left_reduced.len()+1);
+                let right_var = Formula::Var(right_reduced[0].0.clone());
+
+                assignments.push((
+                        gen_var(idx),
+                        Formula::And(Box::new(left_var), Box::new(right_var))));
+                assignments.append(&mut left_reduced);
+                assignments.append(&mut right_reduced);
+            },
+            Formula::Constant(ref val) => {
+                assignments.push((
+                        gen_var(idx),
+                        Formula::Constant(*val)));
+            },
+        };
+        assignments
     }
 
     pub fn count_nontrivial_subformulas(&self) -> i32 {
