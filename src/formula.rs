c82a435d6f9e76ca28fc4c591862972e09cbd69c
Abdul Zreika <azre6702@uni.sydney.edu.au>
2019-08-25 14:34:50 -0500

    Added debug formatters for CNF structs.

--- src/formula.rs
+++ src/formula.rs
@@ -8,7 +8,6 @@ pub enum Formula {
     Constant(bool),
 }
 
-#[derive(Debug)]
 pub struct Conjunction {
     disjunctions: Vec<Disjunction>,
 }
@@ -25,7 +24,6 @@ impl Conjunction {
     }
 }
 
-#[derive(Debug)]
 pub struct Disjunction {
     literals: Vec<Literal>,
 }
@@ -42,7 +40,6 @@ impl Disjunction {
     }
 }
 
-#[derive(Debug)]
 pub struct Literal {
     name: String,
     negated: bool,
@@ -84,6 +81,43 @@ impl fmt::Debug for Formula {
                 false => write!(f, "⊥"),
             },
         }
+    }
+}
+
+impl fmt::Debug for Conjunction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("(")?;
+        let mut delim = "";
+        for disjunction in self.disjunctions.iter() {
+            f.write_str(delim)?;
+            write!(f, "{:?}", disjunction)?;
+            delim = " ∧ ";
+        }
+        f.write_str(")")?;
+        Ok(())
+    }
+}
+
+impl fmt::Debug for Disjunction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("(")?;
+        let mut delim = "";
+        for literal in self.literals.iter() {
+            f.write_str(delim)?;
+            write!(f, "{:?}", literal)?;
+            delim = " ∨ ";
+        }
+        f.write_str(")")?;
+        Ok(())
+    }
+}
+
+impl fmt::Debug for Literal {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.negated {
+            f.write_str("¬")?;
+        }
+        write!(f, "{}", self.name)
     }
 }
 
